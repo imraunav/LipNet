64c2a63dbacbea8cacd793f854be8e941e97a30d
imraunav <raun.ghosh@gmail.com>
2023-10-30 20:32:36 +0530

    hyperparameter added

--- hyperparameters.py
+++ hyperparameters.py
@@ -13,4 +13,6 @@ display = 10
 data_type = 'unseen'
 save_prefix = f'weights/LipNet_{data_type}'
 is_optimize = True
+test_step = 1000
 
+weights = 'pretrain/LipNet_unseen_loss_0.44562849402427673_wer_0.1332580699113564_cer_0.06796452465503355.pt'
