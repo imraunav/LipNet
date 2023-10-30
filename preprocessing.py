18227ab05fa9758924db6a764e9cbded92d32b5a
imraunav <raun.ghosh@gmail.com>
2023-10-30 22:51:03 +0530

    resize face image to find face

--- preprocessing.py
+++ preprocessing.py
@@ -2,34 +2,35 @@ import dlib
 import cv2
 import numpy as np
 
-class CTCCoder:
 
-    def __init__(self, start = 1):
+class CTCCoder:
+    def __init__(self, start=1):
         self.vocab = list(" abcdefghijklmnopqrstuvwxyz")
         self.char2int = {c: i for i, c in enumerate(self.vocab, start=start)}
         self.int2char = {i: c for c, i in self.char2int.items()}
 
-    def encode_char(self, charecter:list):
-        return [self.char2int[str(x)] for x in charecter] # just a safeguard, added str
+    def encode_char(self, charecter: list):
+        return [self.char2int[str(x)] for x in charecter]  # just a safeguard, added str
 
-    def decode_char(self, integer:list):
-        return [self.int2char[int(x)] for x in integer] # just a safeguard, added int
-    
+    def decode_char(self, integer: list):
+        return [self.int2char[int(x)] for x in integer]  # just a safeguard, added int
+
     def ctc_arr2txt(self, arr, start=1):
         # self.int2char = {i+start : x for i, x in enumerate(' abcdefghijklmnopqrstuvwxyz')}
         prev = -1
         txt = []
         for n in arr:
-            if(prev != n and n >= start):
-                if(len(txt) > 0 and txt[-1] == ' ' and self.int2char[int(n)] == ' '):
+            if prev != n and n >= start:
+                if len(txt) > 0 and txt[-1] == " " and self.int2char[int(n)] == " ":
                     pass
                 else:
-                    txt.append(self.int2char[int(n)]) # just a safeguard, added int
+                    txt.append(self.int2char[int(n)])  # just a safeguard, added int
             prev = n
-        return ''.join(txt).strip()
+        return "".join(txt).strip()
+
 
 def HorizontalFlip(batch_img, p=0.5):
-    batch_img = np.array(batch_img) # convenience
+    batch_img = np.array(batch_img)  # convenience
     # (T, H, W, C)
     if np.random.random() > p:
         batch_img = batch_img[:, :, ::-1, ...]
@@ -61,9 +62,13 @@ class LipDetector:
     def __init__(self):
         # Load the face detector and shape predictor
         self.detector = dlib.get_frontal_face_detector()
-        self.predictor = dlib.shape_predictor("./dlib/shape_predictor_68_face_landmarks.dat")
+        self.predictor = dlib.shape_predictor(
+            "./dlib/shape_predictor_68_face_landmarks.dat"
+        )
 
     def findlip(self, im, extra=10):
+        # Resize to get better chance of finding a face
+        im = cv2.resize(im, dsize=(1000, 1000))
         # Detect faces in the grayscale image
         grey = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
         faces = self.detector(im)
