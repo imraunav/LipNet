6e153aeb73cb96e11f44b5e0ca87e1f32f019c86
imraunav <raun.ghosh@gmail.com>
2023-11-01 16:02:42 +0530

    multi gpu option

--- jobscript.sh
+++ jobscript.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH -N 1 
-#SBATCH --ntasks-per-node=8 #number of cores per node
+#SBATCH --ntasks-per-node=16 #number of cores per node
 #SBATCH --time=1-00:00:00 
 #SBATCH --job-name=LipNet  #change name of ur job
 #SBATCH --output=output  #change name of ur output file
 #SBATCH --partition=gpu  #there are various partition. U can change various GPUs
-#SBATCH --gres=gpu:1 #same as above
+#SBATCH --gres=gpu:2 #same as above
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=t22104@students.iitmandi.ac.in
 
