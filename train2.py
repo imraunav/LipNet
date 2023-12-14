d7f32537cd83d9759b03f0c1a65dc5b8a457962b
imraunav <raun.ghosh@gmail.com>
2023-12-14 19:53:49 +0530

    pretrained weights

--- train2.py
+++ train2.py
@@ -158,6 +158,7 @@ def main(rank, world_size):
         train_dataset, hyperparameters.batch_size
     )
     model = LipNet().to(rank)
+    model.load_state_dict(torch.load("weights/LipNet_unseen_loss_0.44562849402427673_wer_0.1332580699113564_cer_0.06796452465503355.pt", map_location=rank))
     trainer = TrainerDDP(
         gpu_id=rank,
         model=model,
